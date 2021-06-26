f61db036c71de6f585d05e80c00128d39ed50c65
Afanti <fanjiahe2000@163.com>
2021-06-26 22:51:39 +0000

    skeleton: initialize module layout

---
+++ driver/plugin.go
@@ -0,0 +1,7 @@
+package driver
+
+// implement container storage interface and use chain as volume provider
+
+type Plugin struct {
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/wonderfan/csi-chain
+
+go 1.16
